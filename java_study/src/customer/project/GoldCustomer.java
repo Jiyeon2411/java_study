d91199de3c0ff4da807d65de1ee131bc745c8ded
jiyeon2411 <daz128@naver.com>
2023-05-01 17:56:50 +0900

    추가

---
+++ java_study/src/customer/project/GoldCustomer.java
@@ -0,0 +1,14 @@
+package customer.project;
+
+public class GoldCustomer extends Customer{
+	double saleRatio; //할인율
+	private int agentID; //상담원 아이디
+	@Override
+	public int calcPrice(int price) {
+		// TODO Auto-generated method stub
+		return super.calcPrice(price);
+	}
+	
+	
+	
+}
---
+++ java_study/src/customer/project/VIPCustomer.java
@@ -0,0 +1,18 @@
+package customer.project;
+
+public class VIPCustomer extends Customer {
+	double saleRatio; //할인율
+
+	@Override
+	public int calcPrice(int price) {
+		// TODO Auto-generated method stub
+		return super.calcPrice(price);
+	}
+
+	@Override
+	public String showCustomerInfo() {
+		// TODO Auto-generated method stub
+		return super.showCustomerInfo();
+	}
+	
+}
